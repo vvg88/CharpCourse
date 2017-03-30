8e5e9cadd5014ee39899c421c02d21581fe939e6
grishanin_vv <Grishanin_VV@Grishanin.neurosoft.ru>
2017-03-30 17:23:12 +0300

    HomeWork3. - Изменена логика программы при вводе пустой строки (выводится сообщение). - Перенесен класс в отдельный файл. - Добавлен метод расчета числа слов. - Убран метод удаления лишних пробелов.

--- HomeWork3/Program.cs
+++ HomeWork3/Program.cs
@@ -13,147 +13,22 @@ namespace HomeWork3
             do
             {
                 Console.Write("Введите строку: ");
-                StringHandler strHandler = new StringHandler(Console.ReadLine());
-                Console.WriteLine("Строка " + (strHandler.IsStrPalindrome() ? "является" : "не является") + " палиндромом.");
-                Console.WriteLine("Число слов в строке: " + strHandler.NumberOfWords);
-                Console.WriteLine("Перевернутая строка: " + strHandler.GetReversedString());
-                Console.WriteLine("Чтобы продолжить, введите любой символ. Для выхода введите q.");
-            }
-            while (Console.ReadLine() != "q");
-        }
-
-        /// <summary>
-        /// Класс обработчик
-        /// </summary>
-        class StringHandler
-        {
-            public StringHandler(string initialStr)
-            {
-                CurrentString = RemoveExtraSpaces(initialStr);
-                strForPalindrome = initialStr.RemoveSigns('.', ',', '!', '?', ' ');     // Для строки, по которой определяется является ли строка палиндромом, удалить знаки препинания и пробелы
-                strForPalindrome = strForPalindrome.ToLower();                          // и привести все буквы к нижнему регистру
-            }
-
-            #region Fields
-            /// <summary>
-            /// Текущая строка
-            /// </summary>
-            private string CurrentString;
-
-            /// <summary>
-            /// Число слов в строке
-            /// </summary>
-            private int wordsInString;
-
-            /// <summary>
-            /// Строка для определения, является ли исходная палиндромом
-            /// </summary>
-            private string strForPalindrome;
-            #endregion
-
-            /// <summary>
-            /// Свойство, которое возвращает число слов в строке
-            /// </summary>
-            public int NumberOfWords
-            {
-                get { return wordsInString; }
-            }
-
-            #region Methods
-            
-            /// <summary>
-            /// Метод определения, является ли строка палиндромом
-            /// </summary>
-            /// <returns> Признак, является ли строка палиндромом </returns>
-            public bool IsStrPalindrome()
-            {
-                bool isPalindrom = true;
-                for (int i = 0, j = strForPalindrome.Length - 1; i < j; i++, j--)
+                string inputString = Console.ReadLine();
+                if (inputString != string.Empty)
                 {
-                    if (strForPalindrome[i] != strForPalindrome[j])
-                    {
-                        isPalindrom = false;
-                        break;
-                    }
-                }
-                return isPalindrom;
-            }
-
-            /// <summary>
-            /// Перевернуть строку задом на перед
-            /// </summary>
-            /// <returns></returns>
-            public string GetReversedString()
-            {
-                var charArray = CurrentString.ToCharArray();
-                for (int i = 0, j = charArray.Length - 1; i < j; i++, j--)
-                {
-                    char tempChar = charArray[i];
-                    charArray[i] = charArray[j];
-                    charArray[j] = tempChar;
-                }
-                return new string(charArray);
-            }
-
-            /// <summary>
-            /// Удалить лишние пробелы между словами
-            /// </summary>
-            /// <param name="str"> Строка </param>
-            /// <returns> Строка без лишних пробелов </returns>
-            private string RemoveExtraSpaces(string str)
-            {
-                var wordsArr = str.Split(new char[] {' '}, StringSplitOptions.RemoveEmptyEntries);
-                wordsInString = wordsArr.Length;        // Сохранить число слов в строке
-                
-                string newStr = string.Empty;       // Создать новую строку без лишних пробелов между словами
-                foreach (var word in wordsArr)
-                    newStr += word + " ";
-                return newStr;
-            }
-            #endregion
-        }
-    }
-
-    /// <summary>
-    /// Методы расширения для работы со строками
-    /// </summary>
-    static class HomeWork3Extention
-    {
-        /// <summary>
-        /// Удалить указанный символ
-        /// </summary>
-        /// <param name="str"> Исходная строка </param>
-        /// <param name="sign"> Удаляемый символ </param>
-        /// <returns> Строка без указанного символа </returns>
-        public static string RemoveSign(this string str, char sign)
-        {
-            string temp = string.Copy(str);
-            for (int i = 0; i < temp.Length;)
-            {
-                if (temp[i] == sign)
-                {
-                    temp = temp.Remove(i, 1);
+                    StringHandler strHandler = new StringHandler(inputString);
+                    //Console.WriteLine("Строка " + (strHandler.IsStrPalindrome() ? "является" : "не является") + " палиндромом.");
+                    Console.WriteLine($"Строка {(strHandler.IsStrPalindrome() ? "является" : "не является")} палиндромом.");
+                    Console.WriteLine($"Число слов в строке: {strHandler.NumberOfWords}");
+                    Console.WriteLine($"Перевернутая строка: {strHandler.GetReversedString()}");
                 }
                 else
                 {
-                    i++;
+                    Console.WriteLine("Пустая строка!");
                 }
+                Console.WriteLine("Чтобы продолжить, введите любой символ. Для выхода введите q.");
             }
-            return temp;
-        }
-
-        /// <summary>
-        /// Удалить несколько знаков из строки
-        /// </summary>
-        /// <param name="str"> Исходная строка </param>
-        /// <param name="signs"> Удаляемые символы </param>
-        /// <returns> Строка без указанных символов </returns>
-        public static string RemoveSigns(this string str, params char[] signs)
-        {
-            string tempStr = string.Copy(str);
-            foreach (var sign in signs)
-                tempStr = tempStr.RemoveSign(sign);
-            return tempStr;
+            while (Console.ReadLine() != "q");
         }
     }
 }
---
+++ HomeWork3/StringHandler.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork3
+{
+    /// <summary>
+    /// Класс обработчик
+    /// </summary>
+    class StringHandler
+    {
+        public StringHandler(string initialStr)
+        {
+            currentString = initialStr;
+            wordsInString = CountWords();
+
+            char[] oddSymbols = initialStr.Where((ch) => { return !char.IsLetterOrDigit(ch); }).ToArray();
+            strForPalindrome = initialStr.RemoveSigns(oddSymbols);     // Для строки, по которой определяется является ли строка палиндромом, удалить знаки препинания, пробелы и прочие лишние символы
+            strForPalindrome = strForPalindrome.ToLower();             // и привести все буквы к нижнему регистру
+        }
+
+        #region Fields
+        /// <summary>
+        /// Текущая строка
+        /// </summary>
+        private string currentString;
+
+        /// <summary>
+        /// Число слов в строке
+        /// </summary>
+        private uint wordsInString;
+
+        /// <summary>
+        /// Строка для определения, является ли исходная палиндромом
+        /// </summary>
+        private string strForPalindrome;
+        #endregion
+
+        /// <summary>
+        /// Свойство, которое возвращает число слов в строке
+        /// </summary>
+        public uint NumberOfWords
+        {
+            get { return wordsInString; }
+        }
+
+        #region Methods
+
+        /// <summary>
+        /// Метод определения, является ли строка палиндромом
+        /// </summary>
+        /// <returns> Признак, является ли строка палиндромом </returns>
+        public bool IsStrPalindrome()
+        {
+            bool isPalindrom = true;
+            for (int i = 0, j = strForPalindrome.Length - 1; i < j; i++, j--)
+            {
+                if (strForPalindrome[i] != strForPalindrome[j])
+                {
+                    isPalindrom = false;
+                    break;
+                }
+            }
+            return isPalindrom;
+        }
+
+        /// <summary>
+        /// Перевернуть строку задом на перед
+        /// </summary>
+        /// <returns></returns>
+        public string GetReversedString()
+        {
+            var charArray = currentString.ToCharArray();
+            for (int i = 0, j = charArray.Length - 1; i < j; i++, j--)
+            {
+                char tempChar = charArray[i];
+                charArray[i] = charArray[j];
+                charArray[j] = tempChar;
+            }
+            return new string(charArray);
+        }
+        
+        /// <summary>
+        /// Метод считает слова в исходной строке
+        /// </summary>
+        /// <returns></returns>
+        private uint CountWords()
+        {
+            List<char> separators = new List<char>();
+
+            foreach (char symb in currentString)
+            {
+                if (!char.IsLetterOrDigit(symb))
+                    separators.Add(symb);
+            }
+
+            return (uint)currentString.Split(separators.ToArray(), StringSplitOptions.RemoveEmptyEntries).Length;
+        }
+        #endregion
+    }
+
+    /// <summary>
+    /// Методы расширения для работы со строками
+    /// </summary>
+    static class HomeWork3Extention
+    {
+        /// <summary>
+        /// Удалить указанный символ
+        /// </summary>
+        /// <param name="str"> Исходная строка </param>
+        /// <param name="sign"> Удаляемый символ </param>
+        /// <returns> Строка без указанного символа </returns>
+        public static string RemoveSign(this string str, char sign)
+        {
+            string temp = string.Copy(str);
+            for (int i = 0; i < temp.Length;)
+            {
+                if (temp[i] == sign)
+                {
+                    temp = temp.Remove(i, 1);
+                }
+                else
+                {
+                    i++;
+                }
+            }
+            return temp;
+        }
+
+        /// <summary>
+        /// Удалить несколько знаков из строки
+        /// </summary>
+        /// <param name="str"> Исходная строка </param>
+        /// <param name="signs"> Удаляемые символы </param>
+        /// <returns> Строка без указанных символов </returns>
+        public static string RemoveSigns(this string str, params char[] signs)
+        {
+            string tempStr = string.Copy(str);
+            foreach (var sign in signs)
+                tempStr = tempStr.RemoveSign(sign);
+            return tempStr;
+        }
+    }
+}
