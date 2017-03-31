e237a9ccd7166dc7e3410cbc0db9f6bd592ca703
vvg88 <grishaninv.v@gmail.com>
2017-03-31 20:50:42 +0300

    HomeWork4 проверка треугольника

--- HomeWork4/Program.cs
+++ HomeWork4/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace HomeWork4
 {
@@ -10,7 +11,19 @@ namespace HomeWork4
     {
         static void Main(string[] args)
         {
+            Shape[] shapesArray = new Shape[]
+            {
+                new Triangle(new Point(1, 2), new Point(5, 2), new Point(3, 6))
+            };
 
+            foreach (var shape in shapesArray)
+            {
+                Console.WriteLine($"Название фигуры: {shape.GetShapeName()}.");
+                Console.WriteLine($"Площадь фигуры: {shape.GetShapeArea()}.");
+                Console.WriteLine($"Периметр фигуры: {shape.GetShapePerimeter()}.");
+            }
+
+            Console.ReadLine();
         }
     }
 }
